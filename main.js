1925781389c323efa1f2177fa29a26afd41fa78d
Thomas Dejardins <tdejardins@gmail.com>
2018-05-08 12:54:00 +0200

    I changed my code on the html and css. And I have put the file main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+document.getElementById("btn").addEventListener("click", myFunction);
+
+function myFunction(){
+
+	
+	var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+
+   if(regex.test(email.value))
+   {
+      
+      document.getElementById("form").style.display = "none";
+      document.querySelector("h1").style.display = "none";
+      var str = document.getElementById("message").innerHTML;
+      var message = str.replace("Enter your email and get 15% off coupon!", "Thank You!");
+      document.getElementById("message").innerHTML = "<h1>" + message + "</h1>";
+
+      document.getElementById("message").style.color = "green";
+
+      document.querySelector("a").style.display = "none";
+   
+      return true;
+   }
+   else
+   {
+      alert("You need put your address email");
+
+      return false;
+   }
+
+}
+
+document.getElementById("link").addEventListener("click", deleteBlocFunction);
+document.getElementById("cross").addEventListener("click", deleteBlocFunction);
+
+function deleteBlocFunction(){
+
+	document.getElementById("bloc").style.display = "none";
+
+}
+
+
+
