555bb9d9654d77ca3c8c3922596d423d4bd4d733
Thomas Dejardins <tdejardins@gmail.com>
2018-05-29 14:09:33 +0200

    Added function validation on js

--- calculator/main.js
+++ calculator/main.js
@@ -1,2 +1,40 @@
 'use strict';
 
+var firstInput = document.getElementById("firstInput");
+var secondInput = document.getElementById("secondInput");
+
+var missFirstField = document.getElementById("missFirstField");
+var missSecondField = document.getElementById("missSecondField");
+
+document.getElementById("form").addEventListener("submit", validation);
+
+function validation(event){
+
+	event.preventDefault();
+
+	if(firstInput.value === ""){
+
+		missFirstField.innerHTML = "Put a number, please"; 
+		missFirstField.style.display = "block";
+		missFirstField.style.color = "red";
+		missFirstField.style.marginLeft = "10px";
+	}
+
+	if(secondInput.value === ""){
+
+		missSecondField.innerHTML = "Put a number, please"; 
+		missSecondField.style.display = "block";
+		missSecondField.style.color = "red";
+		missSecondField.style.marginLeft = "10px";
+	}
+
+	if(firstInput.value === "" || secondInput.value == ""){
+
+		return false;
+	}
+
+	missFirstField.style.display = "none";
+	missSecondField.style.display = "none";
+	
+	return true;
+}
