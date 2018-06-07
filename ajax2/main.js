cb85d181cf852ff8582709cec47e59c94767cf80
Thomas Dejardins <tdejardins@gmail.com>
2018-06-07 17:27:52 +0200

    Added all information in the table

--- ajax2/main.js
+++ ajax2/main.js
@@ -1,37 +1,58 @@
 'use strict';
 
-document.getElementById("show").addEventListener("click", showResult);
-
-document.getElementById("image").style.display = "none";
-
-var cats = {
-	pictures : ""
-};
 
 
-function showResult(){
+	document.getElementById("show").addEventListener("click", showResult);
 
-	var xhttp = new XMLHttpRequest();
+	document.getElementById("image").style.display = "none";
 
-	document.getElementById("image").style.display = "block";
+	document.getElementById("display").style.display = "none";
 
-	xhttp.onreadystatechange = function(){
 
-		if(this.readyState == 4 && this.status == 200){
+	function showResult(){
 
-			document.getElementById("display").innerHTML = xhttp.responseText;
-			document.getElementById("display").style.border = "solid 10px black";
-			document.getElementById("display").style.borderRadius = "20px";
+		var xhttp = new XMLHttpRequest();
 
-			document.getElementById("image").style.display = "none";
+		document.getElementById("image").style.display = "block";
+		
+		xhttp.onreadystatechange = function(){
 
-			localStorage.setItem("Cats",cats);
+			if(this.readyState == 4 && this.status == 200){
 
-			console.log(cats);
+				var obj = document.getElementById("tbody").innerHTML;
+				var response = JSON.parse(xhttp.responseText);
+				var members = response.members;
+
+				for(var i = 0 ; i < members.length ; i++){
+
+					 	obj += "<tr><td>" + members[i].user.avatar + "</td><td>" + members[i].user.avatar_thumbnail 
+					 	+ "</td><td>" + members[i].user.class + "</td><td>" + members[i].user.first_name + "</td><td>" + 
+					 	members[i].user.id + "</td><td>" + members[i].user.last_name + "</td><td>" + members[i].user.page + "</td><td>" 
+					 	+ members[i].user.slug + "</td><td>" + members[i].user.uri + "</td></tr>";
+					 	
+				}
+
+				document.getElementById("tbody").innerHTML = obj;
+
+				document.getElementById("display").style.display = "block";
+				document.getElementById("display").style.border = "solid 10px black";
+				document.getElementById("display").style.borderRadius = "20px";
+
+
+				document.getElementById("image").style.display = "none";
+
+				localStorage.setItem("data-gouv",xhttp.responseText);
+				
+				
+
+				
+			};
 		};
-	};
-	xhttp.open("GET", "http://thecatapi.com/api/images/get?format=xml&results_per_page=20", true);
-	xhttp.send();
+		xhttp.open("GET", "https://www.data.gouv.fr/api/1/organizations/premier-ministre/", true);
+		xhttp.send();
 
-};
+
+	};
+
+
 
