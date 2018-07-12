cc068f6ff6531dd9583e7fa7f57293cedfd70ed2
Thomas Dejardins <tdejardins@gmail.com>
2018-07-12 15:06:38 +0200

    divisible into several functions

--- ajax2/main.js
+++ ajax2/main.js
@@ -1,57 +1,79 @@
 'use strict';
 
 
+document.getElementById("show").addEventListener("click", showResult);
+document.getElementById("show").addEventListener("click", clearTable);
 
-	document.getElementById("show").addEventListener("click", showResult);
-
-	document.getElementById("image").style.display = "none";
-
-	document.getElementById("display").style.display = "none";
+document.getElementById("image").style.display = "none";
+document.getElementById("display").style.display = "none";
 
 
-	function showResult(){
+function showResult(){
 
-		var xhttp = new XMLHttpRequest();
+	var xhttp = new XMLHttpRequest();
 
-		document.getElementById("image").style.display = "block";
+	document.getElementById("image").style.display = "block";
 		
-		xhttp.onreadystatechange = function(){
+	xhttp.onreadystatechange = function(){
 
-			if(this.readyState == 4 && this.status == 200){
+		if(this.readyState == 4 && this.status == 200){
 
-				var obj = document.getElementById("tbody").innerHTML;
-				var response = JSON.parse(xhttp.responseText);
-				var members = response.members;
+			var obj = document.getElementById("tbody").innerHTML;
+			var response = JSON.parse(xhttp.responseText);
+			var members = response.members;
 
-				for(var i = 0 ; i < members.length ; i++){
+			for(var i = 0 ; i < members.length ; i++){
+				var member = members[i];
+				obj += renderMember(member.user);	 	
+			}
 
-					 	obj += "<tr><td><img src='" + members[i].user.avatar + "'></td><td><img src='" + members[i].user.avatar_thumbnail 
-					 	+ "'></td><td>" + members[i].user.class + "</td><td>" + members[i].user.first_name + "</td><td>" + 
-					 	members[i].user.id + "</td><td>" + members[i].user.last_name + "</td><td><a href='" + members[i].user.page + "'>Link</a></td><td>" 
-					 	+ members[i].user.slug + "</td><td><a href='" + members[i].user.uri + "'>Uri</a></td></tr>";
-					 	
-				}
+			clearTable();
 
-				document.getElementById("tbody").innerHTML = obj;
+			document.getElementById("tbody").innerHTML = obj;
 
-				document.getElementById("display").style.display = "block";
-				document.getElementById("display").style.border = "solid 10px black";
-				document.getElementById("display").style.borderRadius = "20px";
-
-
-				document.getElementById("image").style.display = "none";
-
-				localStorage.setItem("data-gouv",xhttp.responseText);
+			display();
+			saveResponse(xhttp.responseText);				
 				
-				
-				
-			};
 		};
-		xhttp.open("GET", "https://www.data.gouv.fr/api/1/organizations/premier-ministre/", true);
-		xhttp.send();
-
-
 	};
+	xhttp.open("GET", "https://www.data.gouv.fr/api/1/organizations/premier-ministre/", true);
+	xhttp.send();
+
+};
+
+
+
+function clearTable(){
+
+	document.getElementById("tbody").innerHTML = "";
+
+};
+
+
+function display(){
+			
+			document.getElementById("image").style.display = "none";
+			document.getElementById("display").style.display = "block";
+};
+
+
+function saveResponse(response){
+
+	localStorage.setItem("data-gouv",response);
+};
+
+
+function renderMember(user){
+
+	user = "<tr><td><img src='" + user.avatar + "'></td><td><img src='" + user.avatar_thumbnail 
+	+ "'></td><td>" + user.class + "</td><td>" + user.first_name + "</td><td>" + 
+	user.id + "</td><td>" + user.last_name + "</td><td><a href='" + user.page + "'><i class='fas fa-link'></i></a></td><td>" 
+	+ user.slug + "</td><td><a href='" + user.uri + "'>Uri</a></td></tr>";
+
+	return user;
+};
+
+
 
 
 
