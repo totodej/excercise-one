11acd18e5c0029c37f2f6652ee35ba0623121d42
Thomas Dejardins <tdejardins@gmail.com>
2018-09-12 14:50:56 +0200

    Find the code to see all informations about users

--- ajax2/main.js
+++ ajax2/main.js
@@ -1,75 +1,159 @@
 'use strict';
 
 
-document.getElementById("show").addEventListener("click", showResult);
-document.getElementById("show").addEventListener("click", clearTable);
+var show = document.querySelector("#show");
+show.addEventListener("click", handleShowClick);
 
-document.getElementById("image").style.display = "none";
-document.getElementById("display").style.display = "none";
+var loader = document.querySelector("#loader");
+loader.style.display = "none";
 
+
+function handleShowClick() {
+	showResult();
+	clearTable();
+}
 
 function showResult(){
 
 	var xhttp = new XMLHttpRequest();
 
-	document.getElementById("image").style.display = "block";
+	loader.style.display = "block";
 		
 	xhttp.onreadystatechange = function(){
 
 		if(this.readyState == 4 && this.status == 200){
 
-			var obj = document.getElementById("tbody").innerHTML;
+			var members = "";
 			var response = JSON.parse(xhttp.responseText);
-			var members = response.members;
+			var users = response;
 
-			for(var i = 0 ; i < members.length ; i++){
-				var member = members[i];
-				obj += renderMember(member.user);	 	
+			for(var i = 0 ; i < users.length ; i++){
+				var user = users[i];
+				members += renderMember(user);	 
+
 			}
 
 			clearTable();
 
-			document.getElementById("tbody").innerHTML = obj;
+			document.getElementById("tbody").innerHTML = members;
 
 			display();
+
+			var detailButtons = document.querySelectorAll(".details");
+			for (var i = 0 ; i < detailButtons.length;i++) {
+				var currentDetailBtn = detailButtons[i];
+				
+				currentDetailBtn.addEventListener('click', function(e){
+					var username = this.getAttribute('data-username');
+					fetchAndRenderUserDetails(username);
+
+				});
+			}
 			saveResponse(xhttp.responseText);				
 				
 		};
 	};
-	xhttp.open("GET", "https://www.data.gouv.fr/api/1/organizations/premier-ministre/", true);
+	xhttp.open("GET", "https://api.github.com/users", true);
 	xhttp.send();
 
 };
 
-
-
 function clearTable(){
-
 	document.getElementById("tbody").innerHTML = "";
-
 };
 
 
 function display(){
-			
-			document.getElementById("image").style.display = "none";
-			document.getElementById("display").style.display = "block";
+	loader.style.display = "none";
+	// document.getElementsByClassName("user")[0].style.display = "block";
 };
-
-
-function saveResponse(response){
-
-	localStorage.setItem("data-gouv",response);
-};
-
 
 function renderMember(user){
-
-	user = "<div class='divTableRow'><div class='divTableCell'><img src='" + user.avatar + "'></div><div class='divTableCell'>" + 
-	user.id + "</div><div class='divTableCell'>" + user.first_name + "</div><div class='divTableCell'>" + user.last_name + "</div><div class='divTableCell'><a href='" + user.page + "'><i class='fas fa-link'></i></a></div></div>";
+	user = "<div class='user'><div id='picture'><img src='" + user.avatar_url + "'></div><div class='informations'><p>" + user.login 
+	+ "</p><div class='fewInformations'></div><a href='" + user.url + 
+	"'><i class='fas fa-user'></i>  See the Profil Page</a></div><button class='details' data-username='"+ user.login +"'>More Details</button></div>";
 
 	return user;
 };
+
+
+
+
+function fetchAndRenderUserDetails(login){
+	var xhttp2 = new XMLHttpRequest();
+
+	xhttp2.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+
+			var user = JSON.parse(xhttp2.responseText);
+			var userDetailsHtml = "<p>Name : " + user.login + "</p><p>Email : " + user.email +  "</p><p>Company : " + user.company + "</p>"; 					
+ 			
+			// render user details
+			var detailButton = document.querySelector('button[data-username="' + user.login +'"]');
+			var fewInformationsElement = detailButton.parentNode.querySelector('.fewInformations');
+			fewInformationsElement.innerHTML = userDetailsHtml;
+		}
+				
+	};
+	xhttp2.open("GET", "https://api.github.com/users/" + login ,true);
+	
+	xhttp2.send();
+};
+
+function userClickEventHandler (login) {
+
+	var details = document.querySelectorAll(".details");
+	var fewInformations = document.querySelectorAll(".fewInformations");
+
+	for(var y = 0 ; y < details.length ; y++ ){
+
+		var detail = details[y];
+
+		detail = login;
+
+		document.querySelectorAll(".user")[y].style.height = "450px";
+ 		document.querySelectorAll(".fewInformations")[y].innerHTML = fewInformations;
+		
+	}
+	
+	console.log(detail);
+
+}
+
+function saveResponse(response){
+   localStorage.setItem("data-gouv",response);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
